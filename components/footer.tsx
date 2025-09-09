b4e0f61ffcd3195eb013da6fee18d1706ce5984c
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-09-09 15:27:37 -0600

    update content details

--- components/footer.tsx
+++ components/footer.tsx
@@ -17,10 +17,10 @@ const Footer = () => {
                 className="mb-4"
               />
               <p className="text-gray-600 mb-4">
-                Más de 18 años brindando soluciones de climatización de calidad para hogares y empresas.
+                Más de 15 años brindando soluciones de climatización de calidad para hogares y empresas.
               </p>
               <div className="flex space-x-4">
-                <a href="https://www.facebook.com/profile.php?id=100069464232203" className="text-gray-500 hover:text-[#EF7632]">
+                <a href="https://www.facebook.com/profile.php?id=100069464232203" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-[#EF7632]">
                   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path
                       fillRule="evenodd"
@@ -39,9 +39,9 @@ const Footer = () => {
                     />
                   </svg>
                 </a> */}
-                <a href="https://x.com/siaacsa" className="text-gray-500 hover:text-[#EF7632]">
+                <a href="https://x.com/siaacsa" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-[#EF7632]">
                   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
                   </svg>
                 </a>
 
@@ -113,13 +113,16 @@ const Footer = () => {
                 <li className="text-gray-600">Sábados: 9:00am - 15:00pm</li>
                 <li className="text-gray-600">Domingos: Cerrado</li>
               </ul>
-             <Link href="https://wa.me/5540852371?text=Hola%2C%20buen%20día.%20Quiero%20una%20cotización..."><Button className="mt-4 bg-[#EF7632] hover:bg-[#d95f15] cursor-pointer">Contactar Ahora</Button></Link>
+             <Link href="https://wa.me/5540852371?text=Hola%2C%20buen%20día.%20Quiero%20una%20cotización..." target="_blank" rel="noopener noreferrer"><Button className="mt-4 bg-[#EF7632] hover:bg-[#d95f15] cursor-pointer">Contactar Ahora</Button></Link>
             </div>
           </div>
 
           <div className="border-t border-gray-200 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-600">© 2025 Proveedora de Climas. Todos los derechos reservados.</p>
             <div className="mt-4 md:mt-0 flex space-x-6">
+              <Link href="#" className="text-gray-600 hover:text-[#EF7632]">
+                Términos y Condiciones
+              </Link>
               <Link href="#" className="text-gray-600 hover:text-[#EF7632]">
                 Política de Privacidad
               </Link>
