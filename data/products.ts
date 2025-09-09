25c3848e610900cf04b670a123523d558d3b38fd
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-09-09 15:35:55 -0600

    update data

--- data/products.ts
+++ data/products.ts
@@ -47,7 +47,7 @@ export const productCategories = [
   }
 ];
 
-{/* productos destacadas de la landig page principal */}
+{/* productos destacados de la landing page principal */}
 export const featuredProducts = [
   {
     id: 1,
@@ -56,7 +56,12 @@ export const featuredProducts = [
     rating: 4.8,
     image: '/products/minisplit-high-wall/minisplit-high-wall.png',
     category: 'expansion-directa',
-    description: 'Le permite alcanzar el máximo ahorro de energía gracias a su alta eficiencia. Capacidades completas garantizadas.'
+    description: 'Le permite alcanzar el máximo ahorro de energía gracias a su alta eficiencia. Capacidades completas garantizadas.',
+    specifications: [
+      'Capacidades disponibles: 1, 1.5 y 3 TR (Frío - Calor)',
+      'Voltaje: 115V (1 TR) y 220V (1-3 TR)',
+      'Refrigerante: R-410A'
+    ]
   },
   {
     id: 2,
@@ -65,7 +70,12 @@ export const featuredProducts = [
     rating: 4.7,
     image: '/products/mini-vrf-diverse/mini-vrf-diverse.png',
     category: 'expansion-directa',
-    description: 'Este novedoso Sistema lntensity Diverse, permite conectar cualquier combinación disponible de Unidades Interiores hasta que lo permita el sistema de selección según cada proyecto, aprovechando la tecnología lnverter para optimizar el consumo de energía y el controlador maestro CCM15 (opcional) para poder manejar cada sistema completo desde un smartphone o tablet.'
+    description: 'Este novedoso Sistema lntensity Diverse, permite conectar cualquier combinación disponible de Unidades Interiores hasta que lo permita el sistema de selección según cada proyecto, aprovechando la tecnología lnverter para optimizar el consumo de energía y el controlador maestro CCM15 (opcional) para poder manejar cada sistema completo desde un smartphone o tablet.',
+    specifications: [
+      'Modelos: Fan & Coil, Cassette 1 vía, Cassette 4 vías, High Wall, Condensadora',
+      'Voltaje de operación: 220V-1F-60Hz',
+      'Refrigerante: R-410A'
+    ]
   },
   {
     id: 3,
@@ -74,7 +84,14 @@ export const featuredProducts = [
     rating: 4.9,
     image: '/products/ChillerModularInverter.png',
     category: 'agua-helada',
-    description: 'La solución más confiable para acondicionar tus espacios'
+    description: 'La solución más confiable para acondicionar tus espacios.',
+    specifications: [
+      'Escalable hasta 320 TR',
+      'Capacidades disponibles: 10 y 20 TR (Frío - Calor)',
+      'Voltaje de operación: 220V-3F-60Hz',
+      'Refrigerante: R-410A',
+      'Operación más precisa y eficiente gracias a sus válvulas de expansión electrónicas'
+    ]
   },
   {
     id: 4,
@@ -83,7 +100,18 @@ export const featuredProducts = [
     rating: 4.8,
     image: '/products/vrf-avantgarde.png',
     category: 'vrf',
-    description: 'Nuestro VRF AVANTGARDE es la solución perfecta para el acondicionamiento de tus espacios.'
+    description: 'Nuestro VRF AVANTGARDE es la solución perfecta para el acondicionamiento de tus espacios.',
+    specifications: [
+      'Descarga vertical',
+      'Compresor Inverter DC de Alta Eficiencia',
+      'Tecnología inteligente de deshielo',
+      'Motores Condensadores DC de Alta Eficiencia',
+      'Posibilidad de conectar 4 módulos para alcanzar 77 TR',
+      'Capacidades disponibles: 6.37, 7.96, 9.56, 11.37, 12.79, 14.33, 15.92, 17.49 y 19.33 TR (Solo frío)',
+      'Voltaje de operación 220V-3F-60Hz y 460V-3F-60Hz',
+      'Refrigerante: R-410A',
+      'Factor de diversidad: 135%'
+    ]
   },
   {
     id: 5,
@@ -92,7 +120,16 @@ export const featuredProducts = [
     rating: 4.6,
     image: '/products/dividido-universal-inverter.png',
     category: 'expansion-directa',
-    description: 'Es la mejor opción para climatizar tu espacio, de uno y dos circuitos según su capacidad.'
+    description: 'Es la mejor opción para climatizar tu espacio, de uno y dos circuitos según su capacidad.',
+    specifications: [
+      'Manejadora horizontal',
+      'Un circuito en 7.5 y 10 TR',
+      'Dos circuitos en 15 y 20 TR',
+      'Controlador incluido',
+      'Capacidades disponibles: 7.5, 10, 15 y 20 TR (solo frio)',
+      'Voltaje: 220V-3F-60Hz',
+      'Refrigerante: R-410A'
+    ]
   },
   {
     id: 6,
@@ -101,23 +138,55 @@ export const featuredProducts = [
     rating: 4.7,
     image: '/products/paquete-inverter-18.png',
     category: 'expansion-directa',
-    description: 'Llega a revolucionar en este tipo de aplicación en el mercado nacional, formando parte de la nueva línea Black Series lntensity. Su gran ahorro energético se debe a la tecnología lnverter en su compresor rotativo y su alta eficiencia de 18 SEER.'
+    description: 'Llega a revolucionar en este tipo de aplicación en el mercado nacional, formando parte de la nueva línea Black Series lntensity. Su gran ahorro energético se debe a la tecnología lnverter en su compresor rotativo y su alta eficiencia de 18 SEER.',
+    specifications: [
+      'Compresor rotativo Inverter',
+      'Motor evaporador tipo ECM y condensador DC Inverter',
+      'Descarga y retorno de aire horizontal',
+      'Recubrimiento anticorrosivo para ambiente salino',
+      'La calefacción opera hasta -15°C',
+      'Capacidades: 3 y 5 TR',
+      'Voltaje: 220V-1F-60Hz',
+      'Refrigerante: R-410A'
+    ]
   },
   {
     id: 7,
     name: 'UNIDAD MANEJADORA ANDRADE',
     brand: 'Intensity',
+    rating: 4.7,
     image: '/products/UMA.png',
     category: 'agua-helada',
-    description: 'Unidad de excelente aceptación en el mercado debido a su fabricación de doble pared con poliuretano inyectado de 25 mm y su capacidad de enfriamiento mejorada gracias a su serpentín de 6 hieleras. ¡Calidad de primer nivel!'
+    description: 'Unidad de excelente aceptación en el mercado debido a su fabricación de doble pared con poliuretano inyectado de 25 mm y su capacidad de enfriamiento mejorada gracias a su serpentín de 6 hieleras. ¡Calidad de primer nivel!',
+    specifications: [
+      'Filtro metálico lavable',
+      'Construida con paneles doble pared, con espesor de 1 pulgada',
+      'Incluye base metálica para montaje',
+      'Termostato digital incluido',
+      'Controlado a 24 voltios, compatible con Termostato WiFi',
+      'Capacidades disponibles: 5, 7.5, 10, 12.5, 20, 25 y 30 TR (Frío - Calor)',
+      'Voltaje de operación: 220V-3F-60Hz de 5 a 30 TR',
+      'Voltaje de operación: 560V-3F-60Hz de 15 a 30 TR (Bajo pedido)',
+      'Agua helada / Agua caliente'
+    ]
   },
   {
     id: 8,
     name: 'MINI CHILLER MODULAR R-32 (1-4 TR)',
     brand: 'Intensity',
+    rating: 4.6,
     image: '/products/MiniChillerModularR32.png',
     category: 'agua-helada',
-    description: 'El Mini Chiller Modular Inverter es un sistema integral que brinda enfriamiento y calefacción confiable durante todo el año; ideal para espacios de crecimiento prograsico.'
+    description: 'El Mini Chiller Modular Inverter es un sistema integral que brinda enfriamiento y calefacción confiable durante todo el año; ideal para espacios de crecimiento prograsico. Puede conectarse a la red local del edificio gracias a su controlador alámbrico inteligente, permitiendo controlar la unidad fácilmente desde cualquier teléfono inteligente.',
+    specifications: [
+      'Incluye controlador inalámbrico',
+      'Opción de recubrimiento anticorrosivo, bajo pedido o para proyectos especiales',
+      'Pueden conectarse hasta 6 módulos',
+      'Escalable hasta 24 TR',
+      'Capacidades disponibles: 1.5 a 4 TR (Frío - Calor)',
+      'Voltaje de operación: 220V-1F-60Hz',
+      'Refrigerante: R32'
+    ]
   }
 ];
 
