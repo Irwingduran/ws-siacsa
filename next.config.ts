98cd57442fe09d01a804863e5811b9c97469a6dc
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-11-05 14:13:25 -0600

    add

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  // variables de entorno pensadas para un neubox 
+  env: {
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID: 'service_buc7lne',
+    NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: 'template_f5zdg5k',
+    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: 'kLCvhJziVsD7K4Xd5',
   }
 };
 
